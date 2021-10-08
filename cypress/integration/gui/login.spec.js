62b29df9906bba8462d6b623bd388cfe65a19a5b
Enzo Giacomelli <egiacomelli09@gmail.com>
2021-10-07 22:12:54 -0300

    aaaaaa

--- cypress/integration/gui/login.spec.js
+++ cypress/integration/gui/login.spec.js
@@ -1,13 +1,17 @@
 /// <reference types="Cypress" />
 
 describe("login", () => {
-    beforeEach(() => cy.visit("https://www.saucedemo.com/"));
+    beforeEach(() => cy.visit("https://www.saucedemo.com/"))
 
     
     it("login blocked user", () => {
         cy.login("locked_out_user", "secret_sauce")
+        const message = "Epic sadface: Sorry, this user has been locked out."
+        cy.xpath("//h3[contains(text(), '"+ message +"')]").should("exist")
+    })
 
-        cy.xpath("h3[contains(text(), " + "Epic sadface: Sorry, this user has been locked out." + ")]").should("exist")
-
+    it("successful login", () =>{
+        cy.login("standard_user", "secret_sauce")
+        cy.get("#react-burger-menu-btn").should("exist")
     })
 })
